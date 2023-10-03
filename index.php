72f3b21cd3da331dd96d743423c6ee54951e4d67
Krisnaputra15 <41879957+Krisnaputra15@users.noreply.github.com>
2023-10-03 23:52:27 +0700

    fix name submit

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <!-- Created by CodingLab |www.youtube.com/c/CodingLabYT-->
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="UTF-8">
-    <!--<title> Login and Registration Form in HTML & CSS | CodingLab </title>-->
-    <link rel="stylesheet" href="style.css">
-    <!-- Fontawesome CDN Link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
+
+<head>
+  <meta charset="UTF-8">
+  <!--<title> Login and Registration Form in HTML & CSS | CodingLab </title>-->
+  <link rel="stylesheet" href="style.css">
+  <!-- Fontawesome CDN Link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
 <body>
   <div class="container">
     <input type="checkbox" id="flip">
@@ -29,9 +31,9 @@
       </div>
     </div>
     <div class="forms">
-        <div class="form-content">
-          <div class="login-form">
-            <div class="title">Login</div>
+      <div class="form-content">
+        <div class="login-form">
+          <div class="title">Login</div>
           <form action="#">
             <div class="input-boxes">
               <div class="input-box">
@@ -44,15 +46,15 @@
               </div>
               <div class="text"><a href="#">Forgot password?</a></div>
               <div class="button input-box">
-                <input type="submit" value="Submit">
+                <input type="submit" name="submit" value="Submit">
               </div>
               <div class="text sign-up-text">Don't have an account? <label for="flip">Sign up now</label></div>
             </div>
-        </form>
-      </div>
+          </form>
+        </div>
         <div class="signup-form">
           <div class="title">Signup</div>
-        <form action="register.php" method="POST">
+          <form action="register.php" method="POST">
             <div class="input-boxes">
               <div class="input-box">
                 <i class="fas fa-user"></i>
@@ -67,14 +69,15 @@
                 <input type="password" name="password" placeholder="Enter your password" required>
               </div>
               <div class="button input-box">
-                <input type="submit" value="Submit">
+                <input type="submit" name="submit" value="Submit">
               </div>
               <div class="text sign-up-text">Already have an account? <label for="flip">Login now</label></div>
             </div>
-      </form>
-    </div>
-    </div>
+          </form>
+        </div>
+      </div>
     </div>
   </div>
 </body>
+
 </html>
